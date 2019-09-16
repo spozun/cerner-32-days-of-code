128bdb6509efd21f6faaecf8749c0624da23cf3c
spozun <spozun@hotmail.com>
2019-09-16 11:18:23 -0400

    Add files via upload

---
+++ hijack.js
@@ -0,0 +1,14 @@
+//cerner_2^5_2019
+//to be run in node.js
+
+function hijack(a, b){
+    this[a] = b;
+}
+
+function addOne(x){
+  return x+1;
+}
+
+hijack("addOne", function(y){return y+2;});
+
+console.log(addOne(7));  //outputs "9"
