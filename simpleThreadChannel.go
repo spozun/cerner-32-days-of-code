8e2fc70808589f4912e488ff1ec28fcb8088d3bb
spozun <spozun@hotmail.com>
2019-09-19 09:02:53 -0400

    Add files via upload

---
+++ simpleThreadChannel.go
@@ -0,0 +1,33 @@
+// cerner_2^5_2019
+package main
+import (
+  "fmt"
+  "time"
+)
+
+func thread1(c chan<- string) {
+  for {
+    time.Sleep(time.Second * 1)
+    c <- "*        *"
+  }
+}
+
+func thread2(c chan<- string) {
+  for {
+    time.Sleep(time.Second * 3)
+    c <- "**********"
+  }
+}
+
+func main() {
+  var ch chan string = make(chan string)
+  go thread1( ch )
+  go thread2( ch )
+  go func() {
+    for {
+      fmt.Println(<- ch)
+    }
+  }()
+  var input string
+  fmt.Scanln(&input) //freeze main thread, until user hits ENTER
+}
