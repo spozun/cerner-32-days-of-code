1463fab9831b83d7c1f20e0181088eeab8b39b67
spozun <spozun@hotmail.com>
2019-09-20 08:05:16 -0400

    Update REPL.lisp

--- REPL.lisp
+++ REPL.lisp
@@ -1,7 +1,7 @@
 ; cerner_2^5_2019
 
 ; LISP REPL'er in one line ([R]ead [E]val [P]rint [L]oop)
-; Will process an any LISP code as input.
+; Will process any LISP code as input.
 
 (loop (print (eval (read))))
 
