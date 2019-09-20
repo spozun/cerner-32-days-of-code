852781307694de118d11f4b368904eb7886273c2
spozun <spozun@hotmail.com>
2019-09-20 08:04:06 -0400

    Add files via upload

---
+++ REPL.lisp
@@ -0,0 +1,14 @@
+; cerner_2^5_2019
+
+; LISP REPL'er in one line ([R]ead [E]val [P]rint [L]oop)
+; Will process an any LISP code as input.
+
+(loop (print (eval (read))))
+
+; sample input:
+; (defun foo (a b) (+ a b))
+; (foo 3 4)
+
+; sample output:
+; FOO
+; 7
