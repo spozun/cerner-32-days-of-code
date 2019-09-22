d310e466b42b649e36a0bd9880c6114227a28de3
spozun <spozun@hotmail.com>
2019-09-22 10:30:36 -0400

    Add files via upload

---
+++ NodeExpressHttpServer.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('*', function(req, res) {
+
+  var options = {
+    root: __dirname + '/'
+  };
+
+	res.sendFile(req.originalUrl, options);
+});
+
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000!');
+});
